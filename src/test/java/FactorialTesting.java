7dc6135bf3f8c9f54b6bdf449556036b029f4144
VarnikaB <1rn18cs115.varnika@gmail.com>
2022-07-26 17:12:12 +0530

    Other test cases

--- src/test/java/FactorialTesting.java
+++ src/test/java/FactorialTesting.java
@@ -17,6 +17,17 @@ public class FactorialTesting {
         int value = fact.calculate();
         assertEquals(1, value);
     }
+    @Test
+    void ShouldBeFactorialOfTen(){
+        Factorial fact = new Factorial(10);
+        int value = fact.calculate();
+        assertEquals(3628800, value);
+    }
 
-
+    @Test
+    void ShouldBeFactorialOfFour(){
+        Factorial fact = new Factorial(4);
+        int value = fact.calculate();
+        assertEquals(24, value);
+    }
 }
